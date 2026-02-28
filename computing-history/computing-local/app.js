a0efab797bb19aa53a1efb8e50e1ba02c5f42a8b
Graeme Malcolm <gmalc@microsoft.com>
2026-02-28 15:05:10 -0800

    Updateed content filter

--- computing-history/computing-local/app.js
+++ computing-history/computing-local/app.js
@@ -359,18 +359,9 @@ async function handleSend() {
 
         // Check for inappropriate content
         const inappropriateWords = [
-            " steal ", " hurt ", " kill ", " harm ", " theft ", " heist ", 
-            " illegal ", " crime ", " shoot ", " murder ", 
-            " suicide ", " rape ", " genocide ", " sex ", " sexual ",
-            " steal.", " hurt.", " kill.", " harm.", " theft.", " heist.", 
-            " illegal.", " crime.", " shoot.", " murder.", 
-            " suicide.", " rape.", " genocide.", " sex.", " sexual.",
-            " steal?", " hurt?", " kill?", " harm?", " theft?", " heist?", 
-            " illegal?", " crime?", " shoot?", " murder?", 
-            " suicide?", " rape?", " genocide?", " sex?", " sexual?",
-            " steal!", " hurt!", " kill!", " harm!", " theft!", " heist!", 
-            " illegal!", " crime!", " shoot!", " murder!", 
-            " suicide!", " rape!", " genocide!", " sex!", " sexual!"
+            " steal", " hurt", " kill", " harm", " theft", " heist", " knife", "weapon", " sword",
+            " illegal", " crime", " shoot", " murder", " stab ", " gun", " bomb",
+            " suicide", " rape", " genocide", " sex", " sexual"
         ];
         const lowerText = text.toLowerCase();
         const containsInappropriate = inappropriateWords.some(word => 
